d52aeb962cbf54872025714bd660224cfd900ce0
Mattias Gees <mattias.gees@gmail.com>
2024-12-13 13:01:51 +0000

    Update Cilium to latest version and enable Socket LoadBalancer Bypass

--- src/configuration/networking_components/ciium.cr
+++ src/configuration/networking_components/ciium.cr
@@ -2,7 +2,7 @@ class Configuration::NetworkingComponents::Cilium
   include YAML::Serializable
   include YAML::Serializable::Unmapped
 
-  getter chart_version : String = "v1.15.7"
+  getter chart_version : String = "v1.16.4"
 
   def initialize
   end
